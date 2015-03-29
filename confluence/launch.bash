5376ca9324c9a53538fd7357fb5eaec1a9c6e8e4
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-03-28 19:59:50 -0700

    Having trouble expanding DB_JDBC_URL properly.
    Switching to xmlstarlet to edit the document.

--- confluence/launch.bash
+++ confluence/launch.bash
@@ -7,22 +7,24 @@ sudo own-volume
 
 if [ -n "$DATABASE_URL" ]; then
   extract_database_url "$DATABASE_URL" DB /srv/wiki/base/lib
-  DB_JDBC_URL="$(xmlstarlet esc "$DB_JDBC_URL")"
   SCHEMA=''
   if [ "$DB_TYPE" != "mysql" ]; then
     SCHEMA='<schema-name>public</schema-name>'
   fi
 fi
 
+echo JDBC=$DB_JDBC_URL
+
 # replace front-end reverse proxy setting in server.xml
+# as well as DataSource configuration
 cat /srv/wiki/site/conf/server.xml | sed \
   -e "s,@@PROXY_NAME@@,$PROXY_NAME," \
   -e "s,@@PROXY_PORT@@,$PROXY_PORT," \
   -e "s,@@PROXY_SCHEME@@,$PROXY_SCHEME," \
   -e "s,@@DB_USER@@,$DB_USER," \
   -e "s,@@DB_PASSWORD@@,$DB_PASSWORD," \
-  -e "s,@@DB_JDBC_URL@@,$DB_JDBC_URL," \
   -e "s,@@DB_JDBC_DRIVER@@,$DB_JDBC_DRIVER," \
+  | xmlstarlet ed -u "//Resource[@name='jdbc/wiki']/@url" -v "$DB_JDBC_URL" \
   > /tmp/server.xml
 cp /tmp/server.xml /srv/wiki/site/conf/server.xml
 
