4e9c84716e78a4b05c2e3b2816d53e5863bf9dd2
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-03-28 19:50:39 -0700

    Expose configured datasource to simplify setup wizard

--- confluence/launch.bash
+++ confluence/launch.bash
@@ -15,10 +15,20 @@ if [ -n "$DATABASE_URL" ]; then
 fi
 
 # replace front-end reverse proxy setting in server.xml
-cat /srv/wiki/site/conf/server.xml | sed -e "s,@@PROXY_NAME@@,$PROXY_NAME," -e "s,@@PROXY_PORT@@,$PROXY_PORT," -e "s,@@PROXY_SCHEME@@,$PROXY_SCHEME," > /tmp/server.xml
+cat /srv/wiki/site/conf/server.xml | sed \
+  -e "s,@@PROXY_NAME@@,$PROXY_NAME," \
+  -e "s,@@PROXY_PORT@@,$PROXY_PORT," \
+  -e "s,@@PROXY_SCHEME@@,$PROXY_SCHEME," \
+  -e "s,@@DB_USER@@,$DB_USER," \
+  -e "s,@@DB_PASSWORD@@,$DB_PASSWORD," \
+  -e "s,@@DB_JDBC_URL@@,$DB_JDBC_URL," \
+  -e "s,@@DB_JDBC_DRIVER@@,$DB_JDBC_DRIVER," \
+  > /tmp/server.xml
 cp /tmp/server.xml /srv/wiki/site/conf/server.xml
 
-cat /srv/wiki/site/classes/atlassian-user.xml | sed -e "s,@@LDAP_PASSWORD@@,$LDAP_PASSWORD," > /srv/wiki/base/confluence/WEB-INF/classes/atlassian-user.xml
+cat /srv/wiki/site/classes/atlassian-user.xml | sed \
+  -e "s,@@LDAP_PASSWORD@@,$LDAP_PASSWORD," \
+  > /srv/wiki/base/confluence/WEB-INF/classes/atlassian-user.xml
 
 export CATALINA_BASE=/srv/wiki/site
 /srv/wiki/base/bin/catalina.sh run
