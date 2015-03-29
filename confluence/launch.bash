306438d558fd76daa1c2e9ce34c4a729eaade930
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-03-28 19:31:19 -0700

    Trying to get Confluence pickup mechanically-seeded DB connection to no avail.
    If I start with confluence.cfg.xml taken from production, which has <setupStep>complete</setupStep>, Confluence fails to start citing it couldn't find the table named BANDANA.
    
    If I set <setupStep> to some of the earlier phase values, such as setupdata-start, then it ignores properties that I seed in confluence.cfg.xml and demands the user to type it in.
    
    On top of that, Confluence really appears unhappy unless the license is provided. The license is tied to server ID, which is generated fresh every time.
    
     So I'm giving up automating preseeding of confluence.cfg.xml. For now, let's have humans go through the setup wizard.

--- confluence/launch.bash
+++ confluence/launch.bash
@@ -18,6 +18,8 @@ if [ -n "$DATABASE_URL" ]; then
   cat <<END > $CFGXML
 <?xml version="1.0" encoding="UTF-8"?>
 <confluence-configuration>
+  <setupStep>setupdata-start</setupStep>
+  <setupType>custom</setupType>
   <properties>
     <property name="admin.ui.allow.manual.backup.download">true</property>
     <property name="attachments.dir">/srv/wiki/home/attachments</property>
@@ -35,13 +37,15 @@ if [ -n "$DATABASE_URL" ]; then
     <property name="hibernate.connection.username">$DB_USER</property>
     <property name="hibernate.database.lower_non_ascii_supported">true</property>
     <property name="hibernate.dialect">com.atlassian.hibernate.dialect.MySQLDialect</property>
-    <property name="hibernate.setup">true</property>
     <property name="lucene.index.dir">/srv/wiki/home/index</property>
     <property name="retrievalUrl">http://localhost:8081</property>
     <property name="rootPath">/srv/wiki/cache/display</property>
     <property name="userName">$DB_USER</property>
     <property name="password">$DB_PASSWORD</property>
     <property name="webwork.multipart.saveDir">/srv/wiki/home/temp</property>
+    <property name="confluence.setup.server.id">B0NE-GNU6-ZHJG-41PT</property>
+    <property name="confluence.license.hash">xxxx</property>
+    <property name="confluence.license.message">yyyx</property>
 END
 
   if [ -d ${CFGXML}.d ]; then
@@ -52,6 +56,32 @@ END
   </properties>
 </confluence-configuration>
 END
+
+#cat << END > /srv/wiki/home/confluence.cfg.xml
+#<confluence-configuration>
+#  <setupStep>setupdata-start</setupStep>
+#  <setupType>custom</setupType>
+#  <buildNumber>0</buildNumber>
+#  <properties>
+#    <property name="confluence.setup.server.id">B0NE-GNU6-ZHJG-41PT</property>
+#    <property name="confluence.webapp.context.path"></property>
+#    <property name="hibernate.connection.password">$DB_PASSWORD</property>
+#    <property name="hibernate.connection.url">$DB_JDBC_URL</property>
+#    <property name="hibernate.connection.username">$DB_USER</property>
+#    <property name="hibernate.c3p0.acquire_increment">1</property>
+#    <property name="hibernate.c3p0.idle_test_period">100</property>
+#    <property name="hibernate.c3p0.max_size">30</property>
+#    <property name="hibernate.c3p0.max_statements">0</property>
+#    <property name="hibernate.c3p0.min_size">0</property>
+#    <property name="hibernate.c3p0.timeout">30</property>
+#    <property name="hibernate.connection.driver_class">com.mysql.jdbc.Driver</property>
+#    <property name="hibernate.dialect">com.atlassian.hibernate.dialect.MySQLDialect</property>
+#    <property name="lucene.index.dir">/srv/wiki/home/index</property>
+#    <property name="confluence.license.hash">xxxx</property>
+#    <property name="confluence.license.message">yyyx</property>
+#  </properties>
+#</confluence-configuration>
+#END
 fi
 
 # replace front-end reverse proxy setting in server.xml
