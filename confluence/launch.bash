8e3f8e24421a44b8c11f47386a723de5d40f9eae
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-03-29 07:59:05 -0700

    simplified a bit by using the home directory short-cut

--- confluence/launch.bash
+++ confluence/launch.bash
@@ -10,7 +10,7 @@ sudo cron -f &
 sudo own-volume
 
 if [ -n "$DATABASE_URL" ]; then
-  extract_database_url "$DATABASE_URL" DB /srv/wiki/base/lib
+  extract_database_url "$DATABASE_URL" DB ~/base/lib
   SCHEMA=''
   if [ "$DB_TYPE" != "mysql" ]; then
     SCHEMA='<schema-name>public</schema-name>'
@@ -19,7 +19,7 @@ fi
 
 # replace front-end reverse proxy setting in server.xml
 # as well as DataSource configuration
-cat /srv/wiki/site/conf/server.xml | sed \
+cat ~/site/conf/server.xml | sed \
   -e "s,@@PROXY_NAME@@,$PROXY_NAME," \
   -e "s,@@PROXY_PORT@@,$PROXY_PORT," \
   -e "s,@@PROXY_SCHEME@@,$PROXY_SCHEME," \
@@ -28,11 +28,11 @@ cat /srv/wiki/site/conf/server.xml | sed \
   -e "s,@@DB_JDBC_DRIVER@@,$DB_JDBC_DRIVER," \
   | xmlstarlet ed -u "//Resource[@name='jdbc/wiki']/@url" -v "$DB_JDBC_URL" \
   > /tmp/server.xml
-mv  /tmp/server.xml /srv/wiki/site/conf/server.xml
+mv  /tmp/server.xml ~/site/conf/server.xml
 
-cat /srv/wiki/site/classes/atlassian-user.xml | sed \
+cat ~/site/classes/atlassian-user.xml | sed \
   -e "s,@@LDAP_PASSWORD@@,$LDAP_PASSWORD," \
-  > /srv/wiki/base/confluence/WEB-INF/classes/atlassian-user.xml
+  > ~/base/confluence/WEB-INF/classes/atlassian-user.xml
 
 # adds/updates a property to confluence.cfg.xml
 # by first deleting and inserting, it ensures that it works correctly even if the file didn't contain the entry
@@ -40,7 +40,7 @@ cat /srv/wiki/site/classes/atlassian-user.xml | sed \
 function add-property {
   n="$1"
   v="$2"
-  CFGXML=/srv/wiki/home/confluence.cfg.xml
+  CFGXML=~/home/confluence.cfg.xml
   cat $CFGXML \
   | xmlstarlet ed -d "//property[@name='$n']" \
   | xmlstarlet ed -s "/confluence-configuration/properties" -t elem -n prop -v "$v" \
@@ -52,7 +52,7 @@ function add-property {
 
 echo DB=$DB_JDBC_URL
 
-CFGXML=/srv/wiki/home/confluence.cfg.xml
+CFGXML=~/home/confluence.cfg.xml
 if [ -f $CFGXML ]; then
   # if config file already exists, touch up its database config
   add-property hibernate.connection.username "$DB_USER"
@@ -65,7 +65,7 @@ if [ -f $CFGXML ]; then
 fi
 
 # somehow Confluence doesn't seem to create these directories on its own
-mkdir -p /srv/wiki/site/{temp,webapps,work}
+mkdir -p ~/site/{temp,webapps,work}
 
-export CATALINA_BASE=/srv/wiki/site
-/srv/wiki/base/bin/catalina.sh run
+export CATALINA_BASE=~/site
+~/base/bin/catalina.sh run
