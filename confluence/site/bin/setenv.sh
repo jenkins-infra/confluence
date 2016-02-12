5dd8301954030213a66e5e56c04bff16cc69c4c8
R. Tyler Croy <tyler@monkeypox.org>
2016-02-12 15:35:53 -0800

    CI should have caught this

--- confluence/site/bin/setenv.sh
+++ confluence/site/bin/setenv.sh
@@ -3,4 +3,4 @@
 # read the one shipped by Atlassian first
 . /srv/wiki/base/bin/setenv.sh
 
-export JAVA_OPTS="${JAVA_OPTS} -Djava.awt.headless=true -Xms512m -Xmx1024m -XX:MaxPermSize=256m -XX:+HeapDumpOnOutOfMemoryError -XX:OnOutOfMemoryError='kill -9 %p'
+export JAVA_OPTS="${JAVA_OPTS} -Djava.awt.headless=true -Xms512m -Xmx1024m -XX:MaxPermSize=256m -XX:+HeapDumpOnOutOfMemoryError -XX:OnOutOfMemoryError='kill -9 %p'"
