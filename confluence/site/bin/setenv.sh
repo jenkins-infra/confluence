adef468a0d4aea491f144467c230782808215eca
olblak <olivier@vernin.me>
2017-05-02 10:23:54 +0200

    Increase maximum Java Heap size to 8GB

--- confluence/site/bin/setenv.sh
+++ confluence/site/bin/setenv.sh
@@ -3,4 +3,4 @@
 # read the one shipped by Atlassian first
 . /srv/wiki/base/bin/setenv.sh
 set -x
-export JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true -Xms512m -Xmx1024m -XX:MaxPermSize=256m -XX:OnOutOfMemoryError=/srv/wiki/site/bin/oomkill"
+export JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true -Xms4096m -Xmx8192m -XX:MaxPermSize=256m -XX:OnOutOfMemoryError=/srv/wiki/site/bin/oomkill"
