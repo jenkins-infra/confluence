385baee8aa192bea2e3af923343cc46a48c30f54
olblak <olivier@vernin.me>
2017-05-31 14:10:11 +0200

    Update confluence/base/bin/setenv.sh
        * Do not source confluence/base/bin/setenv.sh
    
            Do not use setenv.sh script shipped by atlassian
            as it introduce confusion, between args define in confluence/base/bin/setenv.sh
            and confluence/site/bin/setenv.sh
    
        * Increase max java heap size to 8GB

--- confluence/site/bin/setenv.sh
+++ confluence/site/bin/setenv.sh
@@ -1,6 +1,21 @@
 # sourced by catalina.sh upon start
 
-# read the one shipped by Atlassian first
-. /srv/wiki/base/bin/setenv.sh
+# Don't read the one shipped by Atlassian first
+#. /srv/wiki/base/bin/setenv.sh
+
 set -x
-export JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true -Xms4096m -Xmx8192m -XX:MaxPermSize=256m -XX:OnOutOfMemoryError=/srv/wiki/site/bin/oomkill"
+
+CATALINA_OPTS="
+ -XX:-PrintGCDetails
+ -XX:+PrintGCTimeStamps
+ -XX:-PrintTenuringDistribution
+ -Xloggc:${HOME}/home/logs/gc-$(date +%F_%H-%M-%S).log
+ -XX:+UseGCLogFileRotation
+ -XX:NumberOfGCLogFiles=5
+ -XX:GCLogFileSize=2M
+ -Djava.awt.headless=true
+ -Datlassian.plugins.enable.wait=300
+ -Xms4096m -Xmx8192m -XX:+UseG1GC
+ -XX:OnOutOfMemoryError=/srv/wiki/site/bin/oomkill
+ ${CATALINA_OPTS}
+"
