c1396f85a10b6002408a20ead681bb1e133d866f
olblak <me@olblak.com>
2018-08-24 15:57:02 +0200

    Update several settings based on a default installation

--- confluence/site/bin/setenv.sh
+++ confluence/site/bin/setenv.sh
@@ -5,6 +5,8 @@
 
 set -x
 
+CONFLUENCE_CONTEXT_PATH=$("${JAVA_HOME}/bin/java -jar ${CATALINA_HOME}/bin/confluence-context-path-extractor.jar ${CATALINA_HOME}")
+
 CATALINA_OPTS="
  -XX:-PrintGCDetails
  -XX:+PrintGCTimeStamps
@@ -13,6 +15,12 @@ CATALINA_OPTS="
  -XX:+UseGCLogFileRotation
  -XX:NumberOfGCLogFiles=5
  -XX:GCLogFileSize=2M
+ -XX:G1ReservePercent=20
+ -Dsynchrony.enable.xhr.fallback=true
+ -Dorg.apache.tomcat.websocket.DEFAULT_BUFFER_SIZE=32768
+ -Dconfluence.context.path=${CONFLUENCE_CONTEXT_PATH}
+ -XX:ReservedCodeCacheSize=256m
+ -XX:+UseCodeCacheFlushing
  -Djava.awt.headless=true
  -Datlassian.plugins.enable.wait=300
  -Xms8192m -Xmx8192m -XX:+UseG1GC
